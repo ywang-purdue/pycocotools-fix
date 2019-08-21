03d093b873876afa3f98f6939dcf60ff87680f62
junjuew <junjuew@cs.cmu.edu>
2019-08-21 17:40:37 -0400

    symlink common so that c files can be included when building sdist

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -15,16 +15,19 @@ import numpy as np
 ext_modules = [
     Extension(
         'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
+        sources=['./common/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs = [np.get_include(), './common'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
 setup(
     name='pycocotools-fix',
+    author='Junjue Wang',
+    author_email='junjuew@cs.cmu.edu',
     description='Fixed pycocotools package installation error of numpy or cython not installed',
     long_description='Created due to the inactivity of the original repo: https://github.com/cocodataset/cocoapi',
+    url='https://github.com/junjuew/cocoapi',
     packages=['pycocotools'],
     package_dir = {'pycocotools': 'pycocotools'},
     install_requires=[
@@ -36,6 +39,6 @@ setup(
         'cython>=0.27.3',
         'numpy'
     ],
-    version='2.0',
+    version='2.0.0.1',
     ext_modules= ext_modules
 )
